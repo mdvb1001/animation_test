913183c5ec7be8522dac356b26b798f772622bf8
maxvanbel <maxvanbel@gmail.com>
2017-04-15 23:40:28 -0500

    restore to original scraper

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+TweenLite.to(".logo", 2, {left: 600});
+
+// window.onload = function(){
+//     var logo = document.getElementById("logo");
+//     TweenLite.to(logo, 1, {left:"632px"});
+// };
